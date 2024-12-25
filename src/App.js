03c1787a154d3fbd55d0212dd857c34839693594
DiceMast3r <babyisbap@gmail.com>
2024-12-25 13:56:08 +0700

    Add a polyline between waypoint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import axios from "axios";
@@ -27,9 +27,15 @@ function App() {
       });
   }, []);
 
+  // Generate an array of [lat, lon] for Polyline
+  const polylinePositions = locations.map((location) => [
+    location.latitude,
+    location.longitude,
+  ]);
+
   return (
     <div style={{ height: "100vh" }}>
-      <MapContainer center={[51.505, -0.09]} zoom={5} style={{ height: "100%", width: "100%" }}>
+      <MapContainer center={[9.503243879785233, 102.83203125]} zoom={6} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -48,6 +54,9 @@ function App() {
             </Popup>
           </Marker>
         ))}
+
+        {/* Draw polyline connecting all waypoints */}
+        <Polyline positions={polylinePositions} color="red" />
       </MapContainer>
     </div>
   );
